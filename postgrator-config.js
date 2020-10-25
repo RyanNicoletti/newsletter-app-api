6a050e7a4fac13e028cf89e50f5961921a244780
Ryan Nicoletti <ryannicoletti@Ryans-MacBook-Pro-2.local>
2020-10-25 09:25:49 -0400

    fixed db config

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,6 +4,6 @@ module.exports = {
   migrationsDirectory: "migrations",
   driver: "pg",
   connectionString: process.env.DATABASE_URL,
-  ssl: !!process.env.SSL,
+  ssl: process.env.DATABASE_URL ? true : false,
   validateChecksums: false,
 };
